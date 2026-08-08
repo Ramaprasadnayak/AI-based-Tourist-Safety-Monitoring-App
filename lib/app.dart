d36a57f081d2736eb48a9b4a8710a44c934e30a9
Ramaprasadnayak <ramprasadnayak999@gmail.com>
2026-08-08 16:14:07 +0530

    modified yaml

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:safeyatra/features/home_screen.dart';
+import 'package:safeyatra/providers/theme_provider.dart';
+import 'package:safeyatra/themes/dark_theme.dart';
+import 'package:safeyatra/themes/light_theme.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    // final themeProvider =Provider.of<ThemeProvider>(context);
+    final themeProvider =Provider.of<ThemeProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      // theme: lightTheme,
-      // darkTheme: darkTheme,
-      // themeMode: themeProvider.themeMode,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      themeMode: themeProvider.themeMode,
       home:HomeScreen(),
     );
   }
